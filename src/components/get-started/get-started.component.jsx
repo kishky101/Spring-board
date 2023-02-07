93d7ee45220ae0c31dd6d90d1d680bd4ccea8e02
kishky101 <kishky.101@gmail.com>
2023-02-07 14:27:13 +0100

    single page animation complete

--- src/components/get-started/get-started.component.jsx
+++ src/components/get-started/get-started.component.jsx
@@ -1,19 +1,28 @@
 import { Link } from 'react-router-dom';
 import Button from '../button/button.component';
+import { motion } from 'framer-motion';
+import { scaleIn } from '../../utils/framer-motion/framer';
 
 import './get-started.styles.scss'
 
 const GetStarted = () => {
 
     return(
-        <div className='get-started-container'>
-            <div className='get-started-text'>
+        <div
+
+        className='get-started-container'>
+            <motion.div
+            initial='hidden'
+            whileInView='show'
+            variants={scaleIn('up', 0.01)}
+            viewport={{once: false}}
+            className='get-started-text'>
                 <h3>get started now!</h3>
                 <p>Register for any of our interesting course now</p>
                 <Link to='Register'>
                     <Button buttonType={'defaultWhite'} >Register Now</Button>
                 </Link>
-            </div>
+            </motion.div>
         </div>
     )
 }
